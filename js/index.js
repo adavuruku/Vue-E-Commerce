049f746e1cb666d0705ec95905d56f25a56ed4c4
DESKTOP-J6P0TNG\SHERIFF <aabdulraheemsherif@gmail.com>
2020-11-15 19:17:30 +0100

    done with work

--- js/index.js
+++ js/index.js
@@ -70,6 +70,8 @@ var vue_det = new Vue({
             // vue_det.$emit('open_step_two')
             // this.$root.$emit('open_step_two')
             vue_det_two.steptwo = true
+            vue_det_two.totalCost = shoppingCart.map(ele=>ele.price * ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
+            vue_det_two.no_lesson = shoppingCart.length
         }
     }
  });
@@ -80,29 +82,113 @@ var vue_det = new Vue({
     data: {
         steptwo:false,
         cartList: shoppingCart,
-        showcat:true
+        no_lesson:0,
+        errors:[],
+        phoneno:null,
+        fullname:null,
+        totalCost:0,
+        success:false,
+        danger:true,
+        showcat:true,
+        validinput:false
     },
     methods:{
         getImgUrl:(item)=>'resource/'+item.image,
         removeFromCart: function(index){
             vue_det.no_cart -= shoppingCart[index].space
             shoppingCart.splice(index, 1);
+            this.totalCost = shoppingCart.map(ele=>ele.price * ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
+            this.no_lesson = shoppingCart.length
             this.$forceUpdate();
         },
         increaseCart: function(index){
             var count = shoppingCart[index].space
             shoppingCart[index].space = count < 5 ? count +=1 : count
             vue_det.no_cart = shoppingCart.map(ele=>ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
+            this.totalCost = shoppingCart.map(ele=>ele.price * ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
             this.$forceUpdate();
         },
         decreaseCart: function(index){
             var count = shoppingCart[index].space
             shoppingCart[index].space = count > 0 ? count -=1 : count
             vue_det.no_cart = shoppingCart.map(ele=>ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
+            this.totalCost = shoppingCart.map(ele=>ele.price * ele.space).reduce(( accumulator, currentValue ) => accumulator + currentValue,0)
             if(shoppingCart[index].space<=0){
                 this.removeFromCart(index)
             }
+            
             this.$forceUpdate();
+        },
+        submitInput: function(e){
+            e.preventDefault()
+            this.errors=[]
+            if (this.phoneno && this.fullname) {
+                if (this.phoneno.length === 11 && this.fullname.length >= 5 && /\s/.test(this.fullname)){
+                    this.errors.push('Your Order Has Been Submitted Successfully !!')
+                    this.success = true
+                    this.danger = false
+                    return true;
+                }
+            }
+        },
+        validateInput: function(e){
+            if (this.phoneno && this.fullname) {
+                if (this.phoneno.length === 11 && this.fullname.length >= 5 && /\s/.test(this.fullname)){
+                    this.errors=[]
+                    this.validinput = true
+                    return true;
+                }
+            }
+            
+            this.errors = [];
+            this.validinput = false;
+            if (!this.phoneno) {
+              this.errors.push('Provide A Phone Number.');
+            }
+            if (this.phoneno) {
+                if(this.phoneno.length < 11){
+                    this.errors.push('Phone Number is Invalid.');
+                }
+            }
+            
+            if (!this.fullname) {
+              this.errors.push('Provide Your Full Name.');
+            }
+            if (this.fullname) {
+                if(this.fullname.length < 5  || ! /\s/.test(this.fullname)){
+                    this.errors.push('Full Name is Invalid (5 Characters Min. & space).');
+                }
+            }
+           
+            e.preventDefault()
+        },
+        preventText:(evt)=>{
+            //console.log('I dey here')
+            try {
+                evt = (evt) ? evt : window.event;
+                var charCode = (evt.which) ? evt.which : evt.keyCode;
+                if ((charCode > 32 && (charCode < 48 || charCode > 57))) {
+                  evt.preventDefault();;
+                } else {
+                  return true;
+                }
+            }catch (err) {
+                alert(err.Description);
+            }
+
+        },
+        preventNumber:(evt)=>{
+            try {
+                evt = (evt) ? evt : window.event;
+                var charCode = (evt.which) ? evt.which : evt.keyCode;
+                //upper case                            lowercase                              space
+                if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 32) { // space
+                    return true;
+                }
+                evt.preventDefault();
+            }catch (err) {
+                alert(err.Description);
+            }
         },
         openStepOne: function(){
             this.steptwo = false
